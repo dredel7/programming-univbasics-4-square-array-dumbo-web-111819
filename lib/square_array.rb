811de62aaae25f4e98352c71d77b5000e1817dc4
Andrae Delisser <delisserad@gmail.com>
2019-10-23 01:17:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,9 +5,9 @@ def square_array(array)
  
   new_arr=[]
 while counter<array.length()
-  new_arr.push(array[counter] * arraycounter])
+  new_arr.push(array[counter] * array[counter])
   counter+=1
   
   end
-   retutn new_arr
+   return new_arr
 end
