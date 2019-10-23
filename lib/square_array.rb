8c03a01ec003609bf081a913e6b71dcbf27900dc
Andrae Delisser <delisserad@gmail.com>
2019-10-23 01:10:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  
+  counter =0
+  numbers=[1, 3, 4, 5, 6, 7]
+  new_arr=[]
+while counter<array.length()
+  new_arr.push(numbers)
+  counter+=1
+  
+  end
 end
